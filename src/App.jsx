350e8673962374fce0694a4aa9e12c039e6c9565
MoizInayat <123601592+MoizInayat@users.noreply.github.com>
2024-08-12 23:17:49 -0600

    making search bar

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return <div  className= "max-w-[1240px] shadow-xl h-[400px] mx-auto p-3">
+  <input type="Search" className="w-full border border-black rounded text-slate-400 p-4"/>
+</div>;
+}
+
+export default App;
